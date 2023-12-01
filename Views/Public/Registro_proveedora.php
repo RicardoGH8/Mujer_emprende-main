410846592776a8d2fa27a53fe18e04c0da600e33
KevinRM2 <124383398+KevinRM2@users.noreply.github.com>
2023-11-30 19:24:12 -0500

    Add files via upload

--- Views/Public/Registro_proveedora.php
+++ Views/Public/Registro_proveedora.php
@@ -35,20 +35,30 @@
                         <input type="text" class="form-control" placeholder="Nombre" required autocomplete="off" name="Nombre" />
                     </div>
                     <!-- Campo para ingresar el correo -->
-                    <div class="form-group">
-                        <h2>Ingresar su correo</h2>
-                        <input type="email" class="form-control" placeholder="Correo" required autocomplete="off" name="Correo" />
-                    </div>
-                    <!-- Campo para ingresar la contraseña -->
-                    <div class="form-group">
-                        <h2>Ingresar su contraseña</h2>
-                        <input type="password" class="form-control" placeholder="Contraseña" required autocomplete="off" name="Contraseña" />
-                    </div>
-                    <!-- Campo para ingresar el nombre de usuario -->
-                    <div class="form-group">
-                        <h2>Ingresar su nombre de usuario</h2>
-                        <input type="text" class="form-control" placeholder="Usuario" required autocomplete="off" name="Usuario" />
-                    </div>
+                    <!-- Campo para ingresar el nombre(s) -->
+<div class="form-group">
+    <h2>Ingresar su nombre(s)</h2>
+    <input type="text" class="form-control" placeholder="Nombre" required autocomplete="off" name="Nombre" pattern=".*\S+.*">
+</div>
+
+<!-- Campo para ingresar el correo -->
+<div class="form-group">
+    <h2>Ingresar su correo</h2>
+    <input type="email" class="form-control" placeholder="Correo" required autocomplete="off" name="Correo" pattern=".*\S+.*">
+</div>
+
+<!-- Campo para ingresar la contraseña -->
+<div class="form-group">
+    <h2>Ingresar su contraseña</h2>
+    <input type="password" class="form-control" placeholder="Contraseña" required autocomplete="off" name="Contraseña" pattern=".*\S+.*">
+</div>
+
+<!-- Campo para ingresar el nombre de usuario -->
+<div class="form-group">
+    <h2>Ingresar su nombre de usuario</h2>
+    <input type="text" class="form-control" placeholder="Usuario" required autocomplete="off" name="Usuario" pattern=".*\S+.*">
+</div>
+
                     <!-- Botón de registro con color personalizado -->
                     <button type="submit" class="btn btn-primary btn-block" value="$Insertarcuenta" name="enviar">Registrarme</button>
                 </div>
